7e93d5641070827a5db9d33d41d56498d2e3b2ec
Andrew Udvare <audvare@gmail.com>
2022-06-28 00:22:47 -0400

    main: make use of constants

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,45 @@
 #include <QtDBus/QDBusInterface>
 #include <QtNetwork/QNetworkInterface>
 
+#include "constants.h"
 #include "ifupdebug.h"
 #include "specialtypes.h"
 
 static void up() {
-    auto iface = QDBusInterface(QStringLiteral("org.freedesktop.resolve1"),
-                                QStringLiteral("/org/freedesktop/resolve1"),
-                                QStringLiteral("org.freedesktop.resolve1.Manager"),
+    auto iface = QDBusInterface(Strings::DBus::Services::resolve1,
+                                Strings::DBus::Paths::resolve1,
+                                Strings::DBus::Interfaces::resolve1Manager,
                                 QDBusConnection::systemBus());
     if (iface.isValid()) {
-        const auto devIndex = QNetworkInterface::interfaceFromName(
-                                  QString(qEnvironmentVariable("NET_DEVICE")).trimmed())
-                                  .index();
+        const auto devIndex =
+            QNetworkInterface::interfaceFromName(Strings::EnvironmentVariables::netDevice).index();
         LinkDNSIPv4List dnsArgs;
-        for (auto ipAddress : QString(qEnvironmentVariable("DNS_SERVERS"))
-                                  .trimmed()
-                                  .split(QStringLiteral(" "), Qt::SkipEmptyParts)) {
+        for (auto ipAddress : Strings::EnvironmentVariables::dnsServers.split(Strings::singleSpace,
+                                                                              Qt::SkipEmptyParts)) {
             LinkDNSIPv4 add;
             add.family = 2;
-            for (auto val : ipAddress.split(QStringLiteral("."), Qt::SkipEmptyParts)) {
+            for (auto val : ipAddress.split(Strings::period, Qt::SkipEmptyParts)) {
                 add.ipAddress << val.toUInt();
             }
             dnsArgs << add;
         }
         if (dnsArgs.length() > 0) {
             qCDebug(LOG_IFUP_SYSTEMD_RESOLVED)
-                << iface.call(QStringLiteral("SetLinkDNS"), devIndex, QVariant::fromValue(dnsArgs));
+                << iface.call(Strings::DBus::Resolve1::Methods::setLinkDNS,
+                              devIndex,
+                              QVariant::fromValue(dnsArgs));
         }
         LinkDomainsList domainsArg;
-        for (auto domain : QString(qEnvironmentVariable("DNS_SUFFIX"))
-                               .trimmed()
-                               .split(QStringLiteral(" "), Qt::SkipEmptyParts)) {
+        for (auto domain : Strings::EnvironmentVariables::dnsSuffix.split(Strings::singleSpace,
+                                                                          Qt::SkipEmptyParts)) {
             LinkDomains add{domain, false};
             domainsArg << add;
         }
         if (domainsArg.length() > 0) {
-            qCDebug(LOG_IFUP_SYSTEMD_RESOLVED) << iface.call(
-                QStringLiteral("SetLinkDomains"), devIndex, QVariant::fromValue(domainsArg));
+            qCDebug(LOG_IFUP_SYSTEMD_RESOLVED)
+                << iface.call(Strings::DBus::Resolve1::Methods::setLinkDomains,
+                              devIndex,
+                              QVariant::fromValue(domainsArg));
         }
     } else {
         qCCritical(LOG_IFUP_SYSTEMD_RESOLVED) << "Invalid interface!";
